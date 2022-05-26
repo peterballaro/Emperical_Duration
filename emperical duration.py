f9ec7f921725db935cb482ce0681110f8162b953
peterballaro <81711154+peterballaro@users.noreply.github.com>
2022-05-25 22:40:57 -0400

    Adding more stuff

--- emperical duration.py
+++ emperical duration.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from regression import myregression
 import matplotlib.pyplot as plt
+from mympl import mympl
 
 
 df = pd.read_csv('fredgraph.csv')
@@ -12,23 +13,32 @@ df.index = df['Date']
 df.index = pd.to_datetime(df.index)
 df = df[new_columns[1:]].apply(pd.to_numeric, errors='coerce')
 df = df.dropna()
-print(df.head(10))
+
 
 # Regress EFF on 10yr no lag
 x1 = df['EFF'].to_numpy().reshape(len(df['EFF']), 1)
 y1 = df['10YR'].to_numpy().reshape(len(df['10YR']), 1)
 reg1 = myregression(x=x1, y=y1, constant=True)
+dates1 = df.index
+reg_values = [reg1[0] + reg1[1] * eff for eff in x1]
+
+
 print(reg1)
 
 
 roll1 = 10
 x2 = df['EFF'].iloc[roll1:].to_numpy().reshape(len(df['EFF']) - roll1, 1)
 y2 = df['10YR'].shift(roll1).iloc[roll1:].to_numpy().reshape(len(df['10YR'])- roll1, 1)
+
 reg2 = myregression(x=x2, y=y2, constant=True)
 print(reg2)
 
-fig, axes = plt.subplots(1,1, figsize=(12,9))
-axes.plot(df.index, df['EFF'])
-axes.plot(df.index, df['10YR'])
+mympl()
+fig, axes = plt.subplots(1,1)
+#axes.plot(dates1, df['EFF'])
+axes.plot(dates1, df['10YR'])
+axes.plot(dates1, df['EFF'])
+axes.plot(dates1, reg_values )
+
 plt.title('Fuzzy is a bad slark')
 plt.savefig('test_fig')
--- mympl.py
+++ mympl.py
@@ -1,19 +1,62 @@
 import matplotlib as mpl
 import color_constants as cc
+from cycler import cycler
 
-def mympl(backround_color='white', font_family="serif"):
+def mympl(backround_color='white',
+          font_family="serif",
+          vertical=True,
+          horizontal=False):
     ''' Default rcparams'''
 
 
-
+    print(mpl.rcParams.keys())
     mpl.rcParams['axes.spines.right'] = False
     mpl.rcParams['axes.spines.left'] = False
     mpl.rcParams['axes.spines.top'] = False
     mpl.rcParams['axes.spines.bottom'] = True
+    mpl.rcParams['axes.titlelocation'] = 'left'
+
+    mpl.rcParams['axes.titlelocation'] = 'left'
+    mpl.rcParams['axes.titleweight'] = 'bold'
+    mpl.rcParams['axes.titlepad'] = 6.0
+    mpl.rcParams['axes.titlesize'] = 22
+
+    mpl.rcParams['axes.grid'] = True
+    mpl.rcParams['grid.color'] = cc.GRID_COLOR
+    mpl.rcParams['grid.alpha'] = .6
     mpl.rcParams['figure.facecolor'] = backround_color
+    mpl.rcParams['figure.figsize'] = [10, 8]
     mpl.rcParams["axes.facecolor"] = backround_color
     mpl.rcParams["font.family"] = font_family
+    mpl.rcParams['axes.prop_cycle'] = cycler(color=cc.BAR_COLORS)
+    mpl.rcParams['lines.linewidth'] = 2.8
+    mpl.rcParams['grid.linewidth'] =  1.1
 
 
-    #mpl.rcParams['lines.linewidth'] = 2
-    #mpl.rcParams['lines.linestyle'] = '--'
+    mpl.rcParams['xtick.labelsize'] = 16
+    mpl.rcParams['ytick.labelsize'] = 16
+
+    if vertical:
+        mpl.rcParams['axes.grid.axis'] = 'y'
+        mpl.rcParams['ytick.major.pad'] = -21
+
+        mpl.rcParams['ytick.alignment'] = 'bottom'
+        mpl.rcParams['ytick.labelright'] = True
+        mpl.rcParams['ytick.labelleft'] = False
+        mpl.rcParams['ytick.left'] =  True
+
+
+
+
+    elif horizontal:
+        pass
+    else:
+        pass
+
+
+
+# @mpl.rc_context({'lines.linewidth': 3, 'lines.linestyle': '-'})
+# def plotting_function():
+#     plt.plot(data)
+#
+# plotting_function()
